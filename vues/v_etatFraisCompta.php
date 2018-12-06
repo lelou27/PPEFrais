0c955cf1cbd06f80399e23fa387c000702d257f2
leandre <lguegan@coefficy.com>
2018-12-06 13:58:27 +0100

    Mise en place de la verification de fiche de frais et début de validation

--- vues/v_etatFraisCompta.php
+++ vues/v_etatFraisCompta.php
@@ -7,54 +7,66 @@
 
 
     </p>
-    <table class="listeLegere">
-        <caption>Eléments forfaitisés </caption>
-        <tr>
-            <?php
-            foreach ( $lesFraisForfait as $unFraisForfait )
-            {
-                $libelle = $unFraisForfait['libelle'];
-                ?>
-                <th> <?php echo $libelle?></th>
-                <?php
-            }
-            ?>
-        </tr>
-        <tr>
-            <?php
-            foreach (  $lesFraisForfait as $unFraisForfait  )
-            {
-                $quantite = $unFraisForfait['quantite'];
-                ?>
-                <td class="qteForfait"><?php echo $quantite?> </td>
-                <?php
-            }
-            ?>
-        </tr>
-    </table>
-    <table class="listeLegere">
-        <caption>Descriptif des éléments hors forfait -<?php echo $nbJustificatifs ?> justificatifs reçus -
-        </caption>
-        <tr>
-            <th class="date">Date</th>
-            <th class="libelle">Libellé</th>
-            <th class='montant'>Montant</th>
-        </tr>
-        <?php
-        foreach ( $lesFraisHorsForfait as $unFraisHorsForfait )
-        {
-            $date = $unFraisHorsForfait['date'];
-            $libelle = $unFraisHorsForfait['libelle'];
-            $montant = $unFraisHorsForfait['montant'];
-            ?>
+    <?php
+    if (!empty($lesFraisForfait)) {
+    ?>
+        <table class="listeLegere">
+            <caption>Eléments forfaitisés </caption>
             <tr>
-                <td><?php echo $date ?></td>
-                <td><?php echo $libelle ?></td>
-                <td><?php echo $montant ?></td>
+                <?php
+                foreach ( $lesFraisForfait as $unFraisForfait )
+                {
+                    $libelle = $unFraisForfait['libelle'];
+                    ?>
+                    <th> <?php echo $libelle?></th>
+                    <?php
+                }
+                ?>
+            </tr>
+            <tr>
+                <?php
+                foreach (  $lesFraisForfait as $unFraisForfait  )
+                {
+                    $quantite = $unFraisForfait['quantite'];
+                    ?>
+                    <td class="qteForfait"><?php echo $quantite?> </td>
+                    <?php
+                }
+                ?>
+            </tr>
+        </table>
+    <?php
+    } else
+        echo '<h3>Pas de fiche de frais pour ce visiteur ce mois</h3>';
+
+    if (!empty($lesFraisForfait) && !empty($lesFraisHorsForfait)) {
+        ?>
+        <table class="listeLegere">
+            <caption>Descriptif des éléments hors forfait -<?php echo $nbJustificatifs ?> justificatifs reçus -
+            </caption>
+            <tr>
+                <th class="date">Date</th>
+                <th class="libelle">Libellé</th>
+                <th class='montant'>Montant</th>
             </tr>
             <?php
-        }
-        ?>
-    </table>
+            foreach ($lesFraisHorsForfait as $unFraisHorsForfait) {
+                $date = $unFraisHorsForfait['date'];
+                $libelle = $unFraisHorsForfait['libelle'];
+                $montant = $unFraisHorsForfait['montant'];
+                ?>
+                <tr>
+                    <td><?php echo $date ?></td>
+                    <td><?php echo $libelle ?></td>
+                    <td><?php echo $montant ?></td>
+                </tr>
+                <?php
+            }
+            ?>
+        </table>
+        <a href="">Valider la fiche de frais</a>
+        <?php
+    }
+    ?>
 </div>
 </div>
